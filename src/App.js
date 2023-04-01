73ecfb196795eb45e08a9909d04fefbff5d71cc0
Hen Simkin <hensi1@ac.sce.ac.il>
2023-04-01 16:28:13 +0300

    frontend

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useEffect,useState} from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [backendData, setBackendData] = useState(null);
 
-  const [backendData,setBackendData]=useState([{}])
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('http://localhost:3000/');
+      setBackendData(response.data);
+    };
+    fetchData();
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+            <h1>hello</h1>
+          <p>{backendData ? backendData : 'Fetching data...'}</p>
+        </header>
+      </div>
   );
 }
 
 export default App;
+
