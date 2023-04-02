51e6a6281105884b54cbf2978b29f5c8469a411b
Bar <bar3668@gmail.com>
2023-04-03 02:26:46 +0300

    fixed App.test.js problem.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+      <Router>
+        <App />
+      </Router>
+  );
   const linkElement = screen.getByText(/Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consequat libero eget magna commodo, quis pharetra tellus pretium. Sed viverra ante in mauris finibus dapibus. Maecenas congue dapibus nulla, eu gravida orci consequat eu. Phasellus nec nunc malesuada, aliquam massa ac, accumsan metus. Fusce sed dignissim lectus. Nunc elit tellus, sollicitudin ac accumsan ut, egestas et dui. Maecenas aliquam est a ligula scelerisque, in aliquam neque sodales. Nullam condimentum euismod dictum. Curabitur non ex elementum, pretium enim ut, ornare ipsum./i);
   expect(linkElement).toBeInTheDocument();
 });
