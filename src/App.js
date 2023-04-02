b670050236d88d7debc77bd3ff02ad5c16a7a4e3
Omer Halfon <omerhalfon1234@gmail.com>
2023-04-02 20:20:45 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import './index.css';
-import Navbar from './Navbar.js';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="/About.js">About</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </nav>
+    );
+}
 
 function App() {
     return (
@@ -10,8 +22,11 @@ function App() {
             </header>
             <main className="main">
                 <h1 className="title1">Social media</h1>
-                <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consequat libero eget magna commodo, quis pharetra tellus pretium. Sed viverra ante in mauris finibus dapibus. Maecenas congue dapibus nulla, eu gravida orci consequat eu. Phasellus nec nunc malesuada, aliquam massa ac, accumsan metus. Fusce sed dignissim lectus. Nunc elit tellus, sollicitudin ac accumsan ut, egestas et dui. Maecenas aliquam est a ligula scelerisque, in aliquam neque sodales. Nullam condimentum euismod dictum. Curabitur non ex elementum, pretium enim ut, ornare ipsum.</p>
+                <p className="description" style={{ color: 'white' }} >Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consequat libero eget magna commodo, quis pharetra tellus pretium. Sed viverra ante in mauris finibus dapibus. Maecenas congue dapibus nulla, eu gravida orci consequat eu. Phasellus nec nunc malesuada, aliquam massa ac, accumsan metus. Fusce sed dignissim lectus. Nunc elit tellus, sollicitudin ac accumsan ut, egestas et dui. Maecenas aliquam est a ligula scelerisque, in aliquam neque sodales. Nullam condimentum euismod dictum. Curabitur non ex elementum, pretium enim ut, ornare ipsum.</p>
             </main>
+
+
+
         </div>
     );
 }
