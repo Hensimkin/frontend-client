5b1bd25fa3fffdbcf53d3352b70077c9753bbf5e
Omer Halfon <omerhalfon1234@gmail.com>
2023-04-02 19:53:20 +0300

    add background

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
-// import './index.css';
+import './index.css';
 import Navbar from './Navbar.js';
-
 
 function App() {
     return (
         <div>
-            <header>
+            <header className="header">
                 <Navbar />
             </header>
             <main className="main">
-                <h1 className="title1" style={{color: "#e9e5e5"}}>Social media</h1>
-                <p className="description" style={{color: "#e9e5e5"}}>My App</p>
+                <h1 className="title1">Social media</h1>
+                <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consequat libero eget magna commodo, quis pharetra tellus pretium. Sed viverra ante in mauris finibus dapibus. Maecenas congue dapibus nulla, eu gravida orci consequat eu. Phasellus nec nunc malesuada, aliquam massa ac, accumsan metus. Fusce sed dignissim lectus. Nunc elit tellus, sollicitudin ac accumsan ut, egestas et dui. Maecenas aliquam est a ligula scelerisque, in aliquam neque sodales. Nullam condimentum euismod dictum. Curabitur non ex elementum, pretium enim ut, ornare ipsum.</p>
             </main>
         </div>
     );
 }
 
 export default App;
-
