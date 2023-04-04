32fade771c7e64c753d1f0780cdbc31bc4b1cfee
idobe2 <idobe2@ac.sce.ac.il>
2023-04-04 17:49:17 +0300

    Tests errors have been fixed

--- src/tests/test.spec.js
+++ src/tests/test.spec.js
@@ -1,10 +1,10 @@
-import {expect} from 'chai'
+import { expect } from 'chai';
 
-describe("arrays",()=>{
-    describe("#sort",()=>{
-        it("sorting names array",()=>{
-            var names=["adam","hen"];
-            expect(names.sort((a, b) => b.localeCompare(a))).to.be.eql(["hen","adam"]);
-        });
+describe('arrays', () => {
+  describe('#sort', () => {
+    it('sorting names array', () => {
+      const names = ['adam', 'hen'];
+      expect(names.sort((a, b) => b.localeCompare(a))).to.be.eql(['hen', 'adam']);
     });
+  });
 });
