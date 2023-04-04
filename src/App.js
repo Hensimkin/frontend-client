161fc00de0c24c0705d495fc18ba70c3caba9a9b
idobe2 <idobe2@ac.sce.ac.il>
2023-04-04 17:45:33 +0300

    Tests errors have been fixed

--- src/App.js
+++ src/App.js
@@ -15,16 +15,7 @@ function App() {
           <main className="main">
               <h1 className="title1">Social media</h1>
               <p className="description" style={{ color: 'white' }}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  In consequat libero eget magna commodo, quis pharetra tellus pretium.
-                  {/* eslint-disable-next-line max-len */}
-                  Sed viverra ante in mauris finibus dapibus. Maecenas congue dapibus nulla, eu gravida orci consequat eu.
-                  {/* eslint-disable-next-line max-len */}
-                  Phasellus nec nunc malesuada, aliquam massa ac, accused metus. Fusce sed dignissim lectus. Nunc elit tellus,
-                  {/* eslint-disable-next-line max-len */}
-                  sollicitudin ac accumsan ut, egestas et dui. Maecenas aliquam est a ligula scelerisque, in aliquam neque sodales.
-                  {/* eslint-disable-next-line max-len */}
-                  Nullam condimentum euismod dictum. Curabitur non ex elementum, pretium enim ut, ornare ipsum.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat.
               </p>
           </main>
 
--- src/App.test.js
+++ src/App.test.js
@@ -8,6 +8,6 @@ test('renders learn react link', () => {
           <App />
       </Router>,
   );
-  const linkElement = screen.getByText(/Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consequat libero eget magna commodo, quis pharetra tellus pretium. Sed viverra ante in mauris finibus dapibus. Maecenas congue dapibus nulla, eu gravida orci consequat eu. Phasellus nec nunc malesuada, aliquam massa ac, accumsan metus. Fusce sed dignissim lectus. Nunc elit tellus, sollicitudin ac accumsan ut, egestas et dui. Maecenas aliquam est a ligula scelerisque, in aliquam neque sodales. Nullam condimentum euismod dictum. Curabitur non ex elementum, pretium enim ut, ornare ipsum./i);
+  const linkElement = screen.getByText(/Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat./i);
   expect(linkElement).toBeInTheDocument();
 });
