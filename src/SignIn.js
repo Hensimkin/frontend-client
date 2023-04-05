345d1f4bf2a7a6801e6f54c7dc39c7c32c842960
Bar <bar3668@gmail.com>
2023-04-05 11:11:40 +0300

    add connections.

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
-// eslint-disable-next-line import/extensions
-import Navbar from './Navbar.js';
 import './SignIn.css';
-// eslint-disable-next-line import/order
 import { Link } from 'react-router-dom';
-// import Signup from "./Signup.js";
-// import {Route} from "react-router-dom";
+import axios from 'axios';
+import Navbar from './Navbar.js';
 
 function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [serverResponse, setServerResponse] = useState('');
+
+
+  async function post(e) {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5000/post_email', { email });
+      console.log(response.data); // Log the response received from the server
+      const response2 = await axios.post('http://localhost:5000/post_password', { password });
+      console.log(response2.data); // Log the response received from the server
+      setServerResponse(response.data); // Set the response from the server to the state variable
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   function handleEmailChange(event) {
     setEmail(event.target.value);
@@ -18,9 +30,6 @@ function SignIn() {
   function handlePasswordChange(event) {
     setPassword(event.target.value);
   }
-
-  function handleSubmit(event) {
-    event.preventDefault();
 
     // Check email
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -39,45 +48,49 @@ function SignIn() {
     alert(`Email: ${email}\nPassword: ${password}`);
   }
 
-  return (
-      <div>
-          <header className="header">
-              <Navbar />
-          </header>
-          <div className="first_label">
-              <div className="left">
-                  <h1>Sign in</h1>
-                  <br />
-                  <form onSubmit={handleSubmit}>
-                      <label htmlFor="signup_email">
-                          Enter your email:
-                          <input type="text" name="signup_email" value={email} onChange={handleEmailChange} required />
-                      </label>
-                      <label htmlFor="signup_password">
-                          Enter password:
-                          <input className="password" type="password" name="signup_password" value={password} onChange={handlePasswordChange} required />
-                      </label>
-                      <input type="submit" name="signup" value="Sign in" />
-                  </form>
-                  <br />
-                  <br />
-                  {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-                  <label htmlFor="signup">
-                      New user? Sign up
-                      <Link to="/Signup">Here</Link>
-                  </label>
-              </div>
-
-              <div className="right">
-                  <span className="loginwith">Sign in with social network</span>
-                  <button type="button" className="social-signin facebook">Log in with Facebook</button>
-                  <button type="button" className="social-signin twitter">Log in with Twitter</button>
-                  <button type="button" className="social-signin google">Log in with Google+</button>
-              </div>
-
-              <div className="or">OR</div>
+return (
+    <div>
+      <header className="header">
+        <Navbar />
+      </header>
+      <div className="first_label">
+        <div className="left">
+          <h1>Sign in</h1>
+          <br />
+          <form onSubmit={post}>
+            Enter your email:
+            <input type={'text'} value={email} onChange={handleEmailChange} required />
+            Enter password:
+            <input className="password" type="password" value={password} onChange={handlePasswordChange} required />
+            <button type="submit">Sign in</button>
+          </form>
+          <div>
+            <p>{serverResponse}</p>
           </div>
+          <br />
+          <br />
+          <label htmlFor="signup">
+            New user? Sign up
+            <Link to="/Signup">Here</Link>
+          </label>
+        </div>
+
+        <div className="right">
+          <span className="loginwith">Sign in with social network</span>
+          <button type="button" className="social-signin facebook">
+            Log in with Facebook
+          </button>
+          <button type="button" className="social-signin twitter">
+            Log in with Twitter
+          </button>
+          <button type="button" className="social-signin google">
+            Log in with Google+
+          </button>
+        </div>
+
+        <div className="or">OR</div>
       </div>
+    </div>
   );
 }
 
