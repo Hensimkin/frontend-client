e371cb8aa617c066f8b43e17f7fcbbb1e41cccb7
idobe2 <idobe2@ac.sce.ac.il>
2023-04-05 12:47:41 +0300

    Fix linkElement error.

--- src/App.test.js
+++ src/App.test.js
@@ -8,6 +8,6 @@ test('renders learn react link', () => {
           <App />
       </Router>,
   );
-  const linkElement = screen.getByText(/Join the NetConnect community today and start sharing your world with others!./i);
+  const linkElement = screen.getByText('Join the NetConnect community today and start sharing your world with others!');
   expect(linkElement).toBeInTheDocument();
 });
