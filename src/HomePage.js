d53881778d095255eead0e18cf7dd8b2b06f1a4c
Hen Simkin <hensi1@ac.sce.ac.il>
2023-05-15 11:14:16 +0300

    frontend

---
+++ src/HomePage.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+import React, { useState } from 'react';
+import AddProductPopup from './AddProductPopup.js';
+const [popupIsOpen, setPopupIsOpen] = useState(false);
+
+const openPopup = () => {
+  setPopupIsOpen(true);
+};
+const closePopup = () => {
+  setPopupIsOpen(false);
+};
+return (
+  <div className="App">
+    <header className="header">
+      <Navbar />
+    </header>
+    <main className="main">
+      {!popupIsOpen && (
+        <button type="button" onClick={openPopup}>
+          Add Product
+        </button>
+      )}
+      {popupIsOpen && <AddProductPopup closePopup={closePopup} />}
+      {!popupIsOpen && (
+        <>
+          <h1 className="title1">Social media</h1>
+          <p className="description" style={{ color: 'white' }}>
+            Join the NetConnect community today and start sharing your world
+            with others!
+          </p>
+        </>
+      )}
+    </main>
+  </div>
+);
