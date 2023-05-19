eef7ae2f25de5325804a8c6da02ffe2815b7bf30
Bar <bar3668@gmail.com>
2023-05-19 19:26:18 +0300

    add unfollow button

--- src/FollowingList.js
+++ src/FollowingList.js
@@ -1,11 +1,23 @@
 /* eslint-disable */
 import React, { useState } from 'react';
+import './Follow.css'
 
 function FollowingList({ onClose }) {
+  // Dummy data for following
+  const followers = [
+    { id: 1, username: 'user1' },
+    { id: 2, username: 'user2' },
+    { id: 3, username: 'user3' },
+  ];
 
   const handleSubmit = (event) => {
     event.preventDefault();
     onClose();
+  };
+
+  const handleUnfollow = (followerId) => {
+    // Logic to unfollow the user with the provided followerId
+    console.log(`Unfollow user with ID: ${followerId}`);
   };
 
   return (
@@ -14,14 +26,26 @@ function FollowingList({ onClose }) {
         <span className="close-button" onClick={onClose}>
           x
         </span>
-        <h1 className="head">Following</h1>
+        <h1 className="head">Followers</h1>
         <form onSubmit={handleSubmit}>
-          <br></br>
-
-          <br></br>
-          <button type="submit" >Close</button>
+          <ul className="followers-list">
+            {followers.map((follower) => (
+              <li key={follower.id} className="follower-item">
+                <a href={`/${follower.username}`}>{follower.username}</a>
+                <button
+                  type="button"
+                  onClick={() => handleUnfollow(follower.id)}
+                  className="unfollow-button"
+                >
+                  Unfollow
+                </button>
+              </li>
+            ))}
+          </ul>
+          <br />
+          <button type="submit">Close</button>
         </form>
-        <br></br>
+        <br />
       </div>
     </div>
   );
