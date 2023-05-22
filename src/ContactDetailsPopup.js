73b5098b6c705010148b44b84af53f564efbc2fa
Hen Simkin <hensi1@ac.sce.ac.il>
2023-05-22 20:48:58 +0300

    frontend

--- src/ContactDetailsPopup.js
+++ src/ContactDetailsPopup.js
@@ -1,35 +1,44 @@
-// import React from 'react';
-//
-// function ContactDetailsPopup({ user }) {
-//   const handleWhatsAppClick = () => {
-//     const whatsappUrl = `https://wa.me/${user.whatsapp}`;
-//     window.open(whatsappUrl, '_blank');
-//   };
-//
-//   return (
-//       <div className="contact-details-popup">
-//           <h3>Contact Details</h3>
-//           <p>
-//               Name:
-//               {user.name}
-//           </p>
-//           <p>
-//               Email:
-//               {user.email}
-//           </p>
-//           <p>
-//               Phone:
-//               {user.phone}
-//           </p>
-//           <p>
-//               WhatsApp:
-//               <span className="whatsapp-link" onClick={handleWhatsAppClick}>
-//                   {user.whatsapp}
-//               </span>
-//           </p>
-//           {/* Add any other contact details you want to display */}
-//       </div>
-//   );
-// }
-//
-// export default ContactDetailsPopup;
+/* eslint-disable */
+import React from 'react';
+import './ContactDetailsPopup.css';
+
+function ContactDetailsPopup({ listing, closePopup }) {
+  const phoneNumber = listing.phone;
+  const formattedPhoneNumber = phoneNumber.replace(/\D/g, ''); // Remove non-digit characters
+
+  // Function to handle the phone number click and initiate a phone call
+  const handlePhoneNumberClick = () => {
+    window.location.href = `tel:${phoneNumber}`;
+  };
+
+  // Function to handle the WhatsApp click and open a WhatsApp chat
+  const handleWhatsAppClick = () => {
+    window.location.href = `https://wa.me/${formattedPhoneNumber}`;
+  };
+
+  return (
+    <div className="popup">
+      <div className="popup-inner">
+        <h2>Contact Details</h2>
+        <p>
+          Phone Number:
+          <a href={`tel:${phoneNumber}`} onClick={handlePhoneNumberClick}>{phoneNumber}</a>
+        </p>
+        <p>
+          WhatsApp:
+          <a href={`https://wa.me/${formattedPhoneNumber}`} onClick={handleWhatsAppClick}>Send Message</a>
+        </p>
+        <button type="button" onClick={closePopup}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ContactDetailsPopup;
+
+
+
+
+
--- src/HomePage.js
+++ src/HomePage.js
@@ -2,23 +2,22 @@
 import React, { useEffect, useState } from 'react';
 import './index.css';
 import axios from 'axios';
-// import the Navbar component
-// eslint-disable-next-line max-len
-// eslint-disable-next-line import/extensions,import/no-named-as-default,import/no-named-as-default-member
-import AddProductPopup from './AddProductPopup.js';
 import './HomePage.css';
-// eslint-disable-next-line import/extensions
 import UserNavbar from './UserNavbar.js';
+import AddProductPopup from './AddProductPopup.js';
+import ContactDetailsPopup from './ContactDetailsPopup.js';
 
 function HomePage() {
-  const [popupIsOpen, setPopupIsOpen] = useState(false);
+  const [addProductPopupIsOpen, setAddProductPopupIsOpen] = useState(false);
+  const [contactDetailsPopupIsOpen, setContactDetailsPopupIsOpen] = useState(false);
+  const [selectedListing, setSelectedListing] = useState(null);
   const [userListings, setUserListings] = useState([]);
+
   const fetchUserListings = async () => {
     try {
       const response = await axios.get('http://localhost:5000/home_listings');
       setUserListings(response.data);
     } catch (error) {
-      // eslint-disable-next-line no-console
       console.error('Error:', error);
     }
   };
@@ -26,75 +25,75 @@ function HomePage() {
   useEffect(() => {
     fetchUserListings();
   }, []);
-  const openPopup = () => {
-    setPopupIsOpen(true);
+
+  const openAddProductPopup = () => {
+    setAddProductPopupIsOpen(true);
   };
 
-  const closePopup = async () => {
-    setPopupIsOpen(false);
-    // Perform any necessary operations to add the listing
+  const openContactDetailsPopup = (listing) => {
+    setSelectedListing(listing);
+    setContactDetailsPopupIsOpen(true);
+  };
 
-    // Fetch updated user listings
+  const closeAddProductPopup = async () => {
+    setAddProductPopupIsOpen(false);
     await fetchUserListings();
-
-    // Refresh the page
     window.location.reload();
   };
 
+  const closeContactDetailsPopup = () => {
+    setContactDetailsPopupIsOpen(false);
+    setSelectedListing(null);
+  };
+
   return (
-      <div className="App">
-          <header className="header">
-              <UserNavbar />
-          </header>
-          <main className="main">
-              {!popupIsOpen && (
-              <button type="button" onClick={openPopup}>
-                  Add Product
-              </button>
-              )}
-              {popupIsOpen && <AddProductPopup closePopup={closePopup} />}
-              {!popupIsOpen && (
-              <>
-
-              </>
-              )}
-          </main>
-          <div className="listings">
-              <div className="fonts">
-                  <h3>Your Listings</h3>
-                  <ul>
-                      {userListings.map((listing) => (
-                          <li key={listing.id}>
-                              <p>
-                                  Title:
-                                  {listing.title}
-                              </p>
-                              <p>
-                                  Price:
-                                  {listing.price}
-                              </p>
-                              <p>
-                                  Category:
-                                  {listing.category}
-                              </p>
-                              <p>
-                                  Description:
-                                  {listing.description}
-                              </p>
-                            <p>
-                              Phone Number:
-                              {listing.phone}
-                            </p>
-                          </li>
-                      ))}
-                  </ul>
-
-              </div>
-
-          </div>
-
+    <div className="App">
+      <header className="header">
+        <UserNavbar />
+      </header>
+      <main className="main">
+        {!addProductPopupIsOpen && (
+          <button type="button" onClick={openAddProductPopup}>
+            Add Product
+          </button>
+        )}
+        {/* Rest of the code... */}
+      </main>
+      <div className="listings">
+        <div className="fonts">
+          <h3>Your Listings</h3>
+          <ul>
+            {userListings.map((listing) => (
+              <li key={listing.id}>
+                <p>Title: {listing.title}</p>
+                <p>Price: {listing.price}</p>
+                <p>Category: {listing.category}</p>
+                <p>Description: {listing.description}</p>
+                <button
+                  type="button"
+                  onClick={() => openContactDetailsPopup(listing)}
+                  className="button2"
+                >
+                  Contact Details
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
+      {addProductPopupIsOpen && (
+        <AddProductPopup closePopup={closeAddProductPopup} />
+      )}
+      {contactDetailsPopupIsOpen && selectedListing && (
+        <ContactDetailsPopup
+          listing={selectedListing}
+          phoneNumber={selectedListing.phoneNumber}
+          closePopup={closeContactDetailsPopup}
+        />
+      )}
+    </div>
   );
 }
 
 export default HomePage;
+
