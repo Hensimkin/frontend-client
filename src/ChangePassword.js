9ff7709b07b336e27245aba15e88acd8f135c60a
Bar <bar3668@gmail.com>
2023-06-04 20:12:04 +0300

    New password validation

--- src/ChangePassword.js
+++ src/ChangePassword.js
@@ -6,7 +6,10 @@ import axios from 'axios';
 function ChangePassword({ onClose }) {
   const [newPassword, setNewPassword] = useState('');
   const [currentPassword, setCurrentPassword] = useState('');
+  const [ValidNewPassword, setValidNewPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [passwordChanged, setPasswordChanged] = useState(false);
+
 
   const handleChangePassword = async (event) => {
     event.preventDefault();
@@ -14,16 +17,20 @@ function ChangePassword({ onClose }) {
       await axios.post('http://localhost:5000/change_password', {
         newPassword,
         currentPassword,
+        ValidNewPassword,
       });
       console.log('Password has changed successfully');
       setNewPassword(''); // Clear the input fields
       setCurrentPassword(''); // Clear the input fields
+      setValidNewPassword(''); // Clear the input fields
       setErrorMessage(''); // Clear any previous error message
+      setPasswordChanged(true); // Set the passwordChanged state to true
     } catch (error) {
       console.log('Error:', error.response.data);
       setErrorMessage('Failed to change password');
     }
   };
+
 
   return (
     <div className="modal">
@@ -36,19 +43,26 @@ function ChangePassword({ onClose }) {
         <form onSubmit={handleChangePassword}>
           <input
             type="password"
-            placeholder="Current Password"
+            placeholder="Enter Current Password"
             value={currentPassword}
             onChange={(e) => setCurrentPassword(e.target.value)}
           />
           <input
             type="password"
-            placeholder="New Password"
+            placeholder=" Enter New Password"
             value={newPassword}
             onChange={(e) => setNewPassword(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Enter again New Password"
+            value={ValidNewPassword}
+            onChange={(e) => setValidNewPassword(e.target.value)}
           />
           <button type="submit">Change Password</button>
         </form>
         {errorMessage && <p>{errorMessage}</p>}
+        {passwordChanged && <p>Password has changed successfully!</p>}
       </div>
     </div>
   );
