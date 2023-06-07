704e295877f2e36e3a5246d8fb66285384ff6009
Bar <bar3668@gmail.com>
2023-06-07 21:10:00 +0300

    Update edit profile.

--- src/EditProfile.js
+++ src/EditProfile.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+
 import React, { useEffect, useState } from 'react';
 import './EditProfile.css';
 import './ProfilePopup.css';
@@ -11,6 +12,7 @@ function EditProfile({ onClose }) {
   const [PhoneNumber, setPhoneNumber] = useState('');
   const [nameError, setNameError] = useState('');
   const [UserDetails, setUserDetails] = useState([]);
+  const [responseMessage, setResponseMessage] = useState('');
 
   async function post(e) {
     e.preventDefault();
@@ -21,9 +23,10 @@ function EditProfile({ onClose }) {
       );
       const response_phone_number = await axios.post(
         'http://localhost:5000/edit_phone_number',
-        { PhoneNumber },
+        { PhoneNumber: PhoneNumber.slice(1) },
       );
-      console.log(response_name, response_phone_number);
+      const message = `${response_name.data}\n${response_phone_number.data}`;
+      alert(message); // Display the server response in an alert window
       onClose();
     } catch (error) {
       console.log(error);
@@ -61,53 +64,57 @@ function EditProfile({ onClose }) {
   };
 
   return (
-    <div className="popup">
-      <div onClick={onClose} className="overlay" />
-      <div className="popup-inner">
-        <span className="close-button" onClick={onClose}>
-          x
-        </span>
-        <h1 className="head">Edit Profile</h1>
-        <form onSubmit={post}>
-          <br />
-          <label className="fonts">
-            Full Name:
-            <input
-              type="text"
-              id="output"
-              pattern="[a-zA-Z\s]*"
-              value={FullName}
-              onChange={handleFullName}
-            />
-            {nameError && (
-              <span className="error" style={{ color: 'red' }}>
-                {nameError}
+      <div className="popup">
+          <div onClick={onClose} className="overlay" />
+          <div className="popup-inner">
+              <span className="close-button" onClick={onClose}>
+                  x
               </span>
-            )}
-          </label>
-          <br />
-          <label className="fonts">Phone Number:</label>
-          <PhoneInput
-            type="text"
-            value={PhoneNumber}
-            onChange={setPhoneNumber}
-            required
-            defaultCountry="IL"
-          />
-          <br />
-          <label className="fonts">Email: </label>
-          <label className="fonts">
-            {UserDetails.length > 0 ? UserDetails[0].mail : ''}
-          </label>
-          <br />
-          <button type="submit">Save Changes</button>
-        </form>
-        <br />
-        <button type="button" onClick={onClose}>
-          Close
-        </button>
+              <h1 className="head">Edit Profile</h1>
+              <form onSubmit={post}>
+                  <br />
+                  <label className="fonts">
+                      Full Name:
+                      <input
+                        type="text"
+                        id="output"
+                        pattern="[a-zA-Z\s]*"
+                        value={FullName}
+                        onChange={handleFullName}
+                        required
+                      />
+                      {nameError && (
+                      <span className="error" style={{ color: 'red' }}>
+                          {nameError}
+                      </span>
+                      )}
+                  </label>
+                  <br />
+                  <label className="fonts">Phone Number:</label>
+                  <PhoneInput
+                    type="text"
+                    value={PhoneNumber}
+                    onChange={setPhoneNumber}
+                    required
+                    defaultCountry="IL"
+                  />
+                  <br />
+                  <label className="fonts">Email: </label>
+                  <label className="fonts">
+                      {UserDetails.length > 0 ? UserDetails[0].mail : ''}
+                  </label>
+                  <br />
+                  <button type="submit">Save Changes</button>
+              </form>
+              {responseMessage && (
+              <div className="response-message">{responseMessage}</div>
+              )}
+              <br />
+              <button type="button" onClick={onClose}>
+                  Close
+              </button>
+          </div>
       </div>
-    </div>
   );
 }
 
