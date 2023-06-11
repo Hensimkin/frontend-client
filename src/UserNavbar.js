03edadf543bfb16f81a7390531d8e7ba9da086fe
Hen Simkin <hensi1@ac.sce.ac.il>
2023-06-11 20:55:43 +0300

    frontend

--- src/UserNavbar.js
+++ src/UserNavbar.js
@@ -1,27 +1,43 @@
-import React, { useState } from 'react';
+/* eslint-disable */
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './cssFile.css';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faBookmark, faHome, faUser, faSignOutAlt,
+  faBookmark, faHome, faUser, faSignOutAlt, faBell,
 } from '@fortawesome/free-solid-svg-icons';
 
 function UserNavbar() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [showWindow, setShowWindow] = useState(false);
+  const [notifications, setNotifications] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // eslint-disable-next-line no-use-before-define
+    fetchNotifications();
+  }, []);
+
+  const fetchNotifications = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/get_notifications');
+      setNotifications(response.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   const handleLogout = async () => {
     await axios.post('http://localhost:5000/signOut');
   };
 
-  const handleSearchEnter = (event) => {
-    if (event.key === 'Enter') {
-      axios.post('http://localhost:5000/post_signOut');
-      console.log('Search term:', searchTerm);
-    }
+  const handleNotificationClick = () => {
+    setShowWindow(!showWindow);
   };
 
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
+  const handleCloseClick = () => {
+    setShowWindow(false);
   };
 
   return (
@@ -31,25 +47,11 @@ function UserNavbar() {
                   <li className="logo-container">
                       <Link to="/HomePage" className="logo-link">
                           <img
-                            src="http://i.ibb.co/JBSRQjR/output-onlinepngtools.png"
+                            src="https://i.ibb.co/JBSRQjR/output-onlinepngtools.png"
                             alt="Logo"
                             className="logo-image"
                           />
                       </Link>
-                  </li>
-                  <li className="search-bar">
-                      <input
-                        type="text"
-                        placeholder="Search"
-                        value={searchTerm}
-                        onChange={handleSearchChange}
-                        onKeyPress={handleSearchEnter}
-                      />
-                      {/* eslint-disable-next-line react/button-has-type */}
-                      <div className="main">
-                          {/* eslint-disable-next-line react/button-has-type */}
-                      </div>
-
                   </li>
                   <li>
                       <Link to="/Saved">
@@ -65,6 +67,31 @@ function UserNavbar() {
                       <Link to="/PersonalArea">
                           <FontAwesomeIcon icon={faUser} />
                       </Link>
+                  </li>
+                  <li>
+                      {/* eslint-disable-next-line react/button-has-type */}
+                      <button onClick={handleNotificationClick} className="notification-button">
+                          <FontAwesomeIcon icon={faBell} />
+                      </button>
+                      {showWindow && (
+                      <div className="notification-window">
+                          {/* eslint-disable-next-line react/button-has-type */}
+                          <button className="close-button" onClick={handleCloseClick}>×</button>
+                          {loading ? (
+                              <p>Loading notifications...</p>
+                          ) : (
+                              <ul>
+                                  {notifications.map((notifications, index) => (
+
+                                      <li key={index}>
+                                          {notifications}
+                                      </li>
+                                  ))}
+                              </ul>
+
+                          )}
+                      </div>
+                      )}
                   </li>
                   <li>
                       <Link to="/Signin" onClick={handleLogout}>
